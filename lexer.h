81d35709d188569d685bd9f2e90bb54abed77822
alexzzzs <alex3x192837465@gmail.com>
2025-12-03 17:10:41 +1100

    add stuff to lexer.h. added TT_UNKOWN

--- lexer.h
+++ lexer.h
@@ -2,20 +2,19 @@
 #define LEXER_H
 
 typedef enum {
-	TT_IDENT,
-	TT_NUMBER,
-	TT_SEMICOLON,
-	TT_EOF
-	// add more whern needed
+    TT_IDENT,
+    TT_NUMBER,
+    TT_SEMICOLON,
+    TT_EOF,
+    TT_UNKNOWN  // unknown/unexpected character
 } TokenType;
 
 typedef struct {
-	TokenType type; // type of token
-	char *text;   // allocated string for IDENT/NUMBER
+    TokenType type; // type of token
+    char *text;     // allocated string for IDENT/NUMBER or single-character token
 } Token; // Token structure
 
 void lexer_init(void);
-
 Token *next_token(void);
 
 #endif
